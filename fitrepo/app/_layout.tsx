86dee9a50e5e833aa8f687ed59b22c0c0249ba22
DyloreAquino <lutherjerold@gmail.com>
2026-02-25 12:40:03 +0800

    Update root layout to initialize db first

--- fitrepo/app/_layout.tsx
+++ fitrepo/app/_layout.tsx
@@ -1,15 +1,22 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { initDatabase } from '../src/db/migrations'
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
+  // Initialize Database at the start of app
+  useEffect(() => {
+    initDatabase()
+  }, [])
+
   const colorScheme = useColorScheme();
 
   return (
