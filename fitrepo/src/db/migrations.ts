7470f28e8cb1b0c5ca3056cb6868e821a4032151
DyloreAquino <lutherjerold@gmail.com>
2026-02-25 12:38:49 +0800

    Create database schemas for Workout, Exercises, and WE

---
+++ fitrepo/src/db/migrations.ts
@@ -0,0 +1,56 @@
+import { db } from './database'
+
+export function initDatabase() {
+  // Enable foreign key constraints (important in SQLite)
+  db.execSync(`
+    PRAGMA foreign_keys = ON;
+  `)
+
+  // WORKOUTS TABLE
+  db.execSync(`
+    CREATE TABLE IF NOT EXISTS workouts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+      performed_at TEXT NOT NULL,              -- ISO datetime string
+      is_finished INTEGER NOT NULL DEFAULT 0,  -- 0 = false, 1 = true
+
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    );
+  `)
+
+  // EXERCISES TABLE (reusable master list)
+  db.execSync(`
+    CREATE TABLE IF NOT EXISTS exercises (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+      name TEXT NOT NULL,
+      type TEXT NOT NULL CHECK(type IN ('timed', 'reps')),
+
+      image_uri TEXT,
+
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    );
+  `)
+
+  // WORKOUT_EXERCISES TABLE
+  db.execSync(`
+    CREATE TABLE IF NOT EXISTS workout_exercises (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+      workout_id INTEGER NOT NULL,
+      exercise_id INTEGER NOT NULL,
+
+      set_count INTEGER NOT NULL,
+
+      rep_count INTEGER,
+      time_per_set_seconds INTEGER,
+
+      FOREIGN KEY (workout_id)
+        REFERENCES workouts(id)
+        ON DELETE CASCADE,
+
+      FOREIGN KEY (exercise_id)
+        REFERENCES exercises(id)
+    );
+  `)
+}
