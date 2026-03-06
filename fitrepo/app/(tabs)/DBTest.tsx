a5a3588cb57b662bf54164b13e9bf8c9f458083b
DyloreAquino <lutherjerold@gmail.com>
2026-03-06 12:47:21 +0800

    Fixed frontend for tests

--- fitrepo/app/(tabs)/DBTest.tsx
+++ fitrepo/app/(tabs)/DBTest.tsx
@@ -2,27 +2,50 @@ import { useEffect, useState } from 'react'
 import { StyleSheet, View, FlatList, Text } from 'react-native'
 import { supabase } from '../../lib/supabase'
 
+console.log('supabase_client:', supabase)
+
 export default function App() {
-  const [instruments, setInstruments] = useState([])
+  type Workout = {
+    id: number
+    name: string
+    created_at: string
+    performed_at: string
+    is_finished: boolean
+    user_id: string
+  }
+
+  const [workouts, setWorkouts] = useState<Workout[]>([])
 
   useEffect(() => {
-    getInstruments()
+    getWorkouts()
   }, [])
 
-  async function getInstruments() {
-    const { data } = await supabase.from('workouts').select()
-    setInstruments(data)
+  async function getWorkouts() {
+    try {
+      const response = await supabase.from('workouts').select('*')
+      if (response.data) setWorkouts(response.data)
+    } catch(e) {
+      console.log('error:', e)
+    }
   }
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={instruments}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
+      <Text style={{ color: 'black', fontSize: 18, marginBottom: 10 }}>
+        Workouts count: {workouts.length}
+      </Text>
+      
+      {workouts.length === 0 ? (
+        <Text style={{ color: 'red' }}>No workouts found</Text>
+      ) : (
+        <FlatList
+          data={workouts}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Text style={styles.item}>{item.name}</Text>
+          )}
+        />
+      )}
     </View>
   )
 }
