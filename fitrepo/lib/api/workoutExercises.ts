7059c032224fd7ddbe8b04741b288f3ba9199b57
DyloreAquino <lutherjerold@gmail.com>
2026-03-07 15:32:06 +0800

    Added api for workoutExercise

---
+++ fitrepo/lib/api/workoutExercises.ts
@@ -0,0 +1,67 @@
+import { supabase } from '../supabase'
+import { Exercise, WorkoutExercise } from '@/types/database'
+
+export async function getExercisesInWorkout(
+    workout_id: string
+) {
+    const { data, error } = await supabase.from('workout_exercises')
+        .select('*, exercises ( name, type, image_url ) ')
+        .eq('workout_id', workout_id)
+    if (error) throw error
+    return data
+}
+
+export async function getExerciseInWorkout(
+    exercise_id: string,
+    workout_id: string
+) {
+    const { data, error } = await supabase.from('workout_exercises')
+        .select('*, exercises ( name, type, image_url ) ')
+        .eq('workout_id', workout_id)
+        .eq('exercise_id', exercise_id)
+        .single()
+    if (error) throw error
+    return data
+}
+
+export async function addExerciseToWorkout(
+    workout_id: string,
+    exercise_id: string,
+    workout_exercise: Omit<WorkoutExercise, 'id' | 'workout_id' | 'exercise_id'>
+) {
+    const { data, error } = await supabase.from('workout_exercises')
+        .insert({
+            sets: workout_exercise.sets,
+            reps: workout_exercise.reps,
+            time_seconds: workout_exercise.time_seconds,
+            workout_id: workout_id,
+            exercise_id: exercise_id
+        }).select().single()
+    if (error) throw error
+    return data
+}
+
+export async function updateWorkoutExercise(
+    workout_id: string,
+    exercise_id: string,
+    workout_exercise: Partial<WorkoutExercise>
+) {
+    const { data, error } = await supabase.from('workout_exercises')
+        .update(workout_exercise)
+        .eq('workout_id', workout_id)
+        .eq('exercise_id', exercise_id)
+        .select().single()
+    if (error) throw error
+    return data
+}
+
+export async function removeExerciseFromWorkout(
+    exercise_id: string,
+    workout_id: string
+) {
+    const { error } = await supabase.from('workout_exercises')
+    .delete()
+    .eq('workout_id', workout_id)
+    .eq('exercise_id', exercise_id)
+    if (error) throw error
+}
