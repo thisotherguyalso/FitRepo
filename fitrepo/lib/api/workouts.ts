4ce9b777f7b6407e78979b60322a245320af23b7
DyloreAquino <lutherjerold@gmail.com>
2026-03-08 14:07:29 +0800

    Added error check for if adding workout in the same day

--- fitrepo/lib/api/workouts.ts
+++ fitrepo/lib/api/workouts.ts
@@ -16,9 +16,15 @@ export async function getWorkout(id: string) {
 }
 
 // Creates a workout. Only needs name, performed_at, and is_finished.
+// Will throw an error if creating another workout for the same day.
 export async function createWorkout(
     workout: Omit<Workout, 'id' | 'user_id' | 'created_at'>
 ) {
+    // check if workout already exists for today
+    const today = new Date().toISOString().split('T')[0]
+    
+    const { data: existing } = await supabase.from('workouts').select('id').eq('performed_at', today).single()
+    if (existing) throw new Error('You already have a workout for today!')
     const { data, error } = await supabase.from('workouts').insert(workout).select().single()
     if (error) throw error
     return data
