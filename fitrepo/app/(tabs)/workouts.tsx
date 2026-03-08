40c33aa54866534caf2cc799e1f9ca1fe321c3e4
DyloreAquino <lutherjerold@gmail.com>
2026-03-08 14:49:23 +0800

    Mark dates that have workouts in the calendar

--- fitrepo/app/(tabs)/workouts.tsx
+++ fitrepo/app/(tabs)/workouts.tsx
@@ -1,21 +1,45 @@
 import { StyleSheet } from 'react-native';
-
-import { Collapsible } from '@/components/ui/collapsible';
+import { useEffect, useState} from 'react';
+import { Workout } from '@/types/database';
+import { getWorkouts } from '@/lib/api/workouts';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Fonts } from '@/constants/theme';
-import { Calendar, Agenda } from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 
 export default function TabTwoScreen() {
+  const [workouts, setWorkouts] = useState<Workout[]>([])
+
+  useEffect(() => {
+    loadWorkouts()
+  }, [])
+
+  async function loadWorkouts() {
+    const data = await getWorkouts()
+    setWorkouts(data ?? [])
+  }
+
+  const markedDates = workouts.reduce((acc, workout) => {
+    acc[workout.performed_at] = {
+      marked: true,
+      dotColor: workout.is_finished ? '#f5c842' : '#3b82f6'
+    }
+    return acc
+  }, {} as Record<string, any>)
+
   return (
     <>
       <ParallaxScrollView
         headerBackgroundColor={{ light: '#00adccfa', dark: '#020975fb' }}>
         <Calendar
+          markedDates={markedDates}
+          hideExtraDays={false}
+          showSixWeeks={true}
+          current={new Date().toISOString().split('T')[0]}
           theme={{
             backgroundColor: '#01252cfa',
             calendarBackground: '#01252cfa',
+          }}
+          onDayPress={(day) => {
+            console.log('pressed:', day.dateString)
           }}
         />
       </ParallaxScrollView>
